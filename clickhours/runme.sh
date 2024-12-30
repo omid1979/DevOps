f351fb7386c3333f578fb4bd8033c6ec1cfa6448
Omid Hosseini <admin@omid-online.com>
2024-12-30 11:48:49 +0330

    Update runme.sh

--- clickhours/runme.sh
+++ clickhours/runme.sh
@@ -1,5 +1,5 @@
 
-docker run -d --name docker_sandbox -d clickhouse/clickhouse-server:latest
+docker run -d --name clickhours -d clickhouse/clickhouse-server:latest
 
 docker run -d -p 3000:3000 --name=grafana \
   --volume grafana-storage:/var/lib/grafana \
